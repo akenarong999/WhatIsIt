be8a52ec51935c47e55e16d7bf99551e0a853dbd
akenarong999 <ake.9991@gmail.com>
2019-10-14 02:17:52 +0700

    Initial Commit

---
+++ WhatIsIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatIsIt
+//
+//  Created by Summer on 14/10/19.
+//  Copyright © 2019 Ake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
